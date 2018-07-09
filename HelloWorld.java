8af8688f962f291d54bae86a462777117a09461b
siddaram-jamagav <siddaram.jamagav@avekshaa.com>
2018-07-09 16:21:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public static void main(String args []) {
 
 	int i=1;
 	for(int j=1;j<=100;j++) {
+		System.out.println("Demo sample");
 		System.out.println(i);
 		System.out.println("Hello world Example");
 		i++;
